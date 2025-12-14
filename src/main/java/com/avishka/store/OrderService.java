9065e4d031cfff38fca297287686786ca9c4f4e4
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2025-12-14 09:18:35 +0530

    refactor OrderService and PayPalPaymentService for dependency injection; update StoreApplication to use Spring context

--- src/main/java/com/avishka/store/OrderService.java
+++ src/main/java/com/avishka/store/OrderService.java
@@ -1,15 +1,19 @@
 package com.avishka.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
-
-    public void setPaymentService(PaymentService paymentService) {
+    public OrderService(){}
+    @Autowired
+    public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
+
 
     public void placeOrder() {
         System.out.println("Processing order...");
